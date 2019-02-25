3b55eb41dcc2fbc521fe6b515251a91855a880c2
haiancaomin <haiancaomin@126.com>
2019-02-25 17:39:14 +0800

    no message

--- src/page/Index.vue
+++ src/page/Index.vue
@@ -11,7 +11,6 @@
               </el-carousel-item>
             </el-carousel>
           </div>
-		  eee
         </el-col>
         <el-col :span="8">
           <div class="grid-content login-box">
