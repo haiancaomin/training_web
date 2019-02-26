83bdd29a536bf34e6d0378d8854e1bae1a668ca6
haiancaomin <haiancaomin@126.com>
2019-02-26 16:57:19 +0800

    路由修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import UserLogin from '@/components/UserLogin'
-import UserRegister from '@/components/UserRegister'
 import Index from '@/page/Index'
 
 Vue.use(Router)
@@ -9,18 +7,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/UserLogin',
-      name: 'UserLogin',
-      component: UserLogin
-    },
-    {
-      path: '/UserRegister',
-      name: 'UserRegister',
-      component: UserRegister
-    },
-    {
       path: '/',
-      name: 'Index',
       component: Index
     }
   ]
