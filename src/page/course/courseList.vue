f8899cf0afaf89f1b35afe55bd540fefd5db74d9
haiancaomin <haiancaomin@126.com>
2019-02-27 12:20:23 +0800

    课程

--- src/page/course/courseList.vue
+++ src/page/course/courseList.vue
@@ -1,18 +1,203 @@
 <template>
-  <top-nav></top-nav>
+  <el-container>
+    <top-nav></top-nav>
+    <el-main>
+      <div class="course-container">
+        <el-tabs type="border-card" stretch>
+          <el-tab-pane label="生产类">
+            <el-row :gutter="20" class="bottom-line">
+              <el-col :span="6">
+                <div class="grid-content">
+                  <img
+                    src="http://www.axfc.cn/uploads/20190222/8470b0cd14194545b6a04a4768184ef6.jpg"
+                    class="full-width"
+                  >
+                </div>
+              </el-col>
+              <el-col :span="18">
+                <div class="grid-content">
+                  <el-row :gutter="20">
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">课程介绍</div>
+                    </el-col>
+                    <el-col :span="17">
+                      <div
+                        class="grid-content"
+                      >课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介</div>
+                    </el-col>
+                    <el-col :span="4">
+                      <div class="grid-content">
+                        <el-button type="primary">点我报名</el-button>
+                      </div>
+                    </el-col>
+                  </el-row>
+                  <el-row :gutter="20">
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">培训时长</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content">2天</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">授课老师</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content">赵日天</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">其他标题</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content">其他内容</div>
+                    </el-col>
+                  </el-row>
+                </div>
+              </el-col>
+            </el-row>
+            <el-row :gutter="20" class="bottom-line">
+              <el-col :span="6">
+                <div class="grid-content">
+                  <img
+                    src="http://www.axfc.cn/uploads/20190222/8470b0cd14194545b6a04a4768184ef6.jpg"
+                    class="full-width"
+                  >
+                </div>
+              </el-col>
+              <el-col :span="18">
+                <div class="grid-content">
+                  <el-row :gutter="20">
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">课程介绍</div>
+                    </el-col>
+                    <el-col :span="17">
+                      <div
+                        class="grid-content"
+                      >课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介</div>
+                    </el-col>
+                    <el-col :span="4">
+                      <div class="grid-content">
+                        <el-button type="primary">点我报名</el-button>
+                      </div>
+                    </el-col>
+                  </el-row>
+                  <el-row :gutter="20">
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">培训时长</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content">2天</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">授课老师</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content">赵日天</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">其他标题</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content">其他内容</div>
+                    </el-col>
+                  </el-row>
+                </div>
+              </el-col>
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="施工类">
+            <el-row :gutter="20" class="bottom-line">
+              <el-col :span="6">
+                <div class="grid-content">
+                  <img
+                    src="http://www.axfc.cn/uploads/20190222/8470b0cd14194545b6a04a4768184ef6.jpg"
+                    class="full-width"
+                  >
+                </div>
+              </el-col>
+              <el-col :span="18">
+                <div class="grid-content">
+                  <el-row :gutter="20">
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">课程介绍</div>
+                    </el-col>
+                    <el-col :span="17">
+                      <div
+                        class="grid-content"
+                      >课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介课程简介</div>
+                    </el-col>
+                    <el-col :span="4">
+                      <div class="grid-content">
+                        <el-button type="primary">点我报名</el-button>
+                      </div>
+                    </el-col>
+                  </el-row>
+                  <el-row :gutter="20">
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">培训时长</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content">2天</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">授课老师</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content">赵日天</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content bold text-right">其他标题</div>
+                    </el-col>
+                    <el-col :span="3">
+                      <div class="grid-content">其他内容</div>
+                    </el-col>
+                  </el-row>
+                </div>
+              </el-col>
+            </el-row>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </el-main>
+  </el-container>
 </template>
 <script>
 import TopNav from "../../components/TopNav";
 
 export default {
-  data(){
-    return {
-
-    }
+  data() {
+    return {};
   },
-  components:{
+  components: {
     TopNav
   }
-}
+};
 </script>
+
+<style scoped>
+.el-main {
+  padding-top: 60px;
+}
+.el-row {
+  padding: 20px 0;
+}
+.bottom-line {
+  border-bottom: 1px solid #eee;
+}
+.course-container {
+  padding: 20px;
+  background: #fff;
+  margin-top: 20px;
+}
+.full-width {
+  width: 100%;
+}
+.bold {
+  font-weight: bold;
+}
+.text-right {
+  text-align: right;
+}
+</style>
+
+
 
