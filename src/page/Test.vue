26be866a7d63307b3c3b2afa1f0ea591f7ac10a9
yanweiyuan <2569302699@qq.com>
2019-03-04 17:17:45 +0800

    上传test

---
+++ src/page/Test.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="test">
+      <p>sss</p>
+  <p>sss</p>
+  <p>sss</p>
+  <p>sss</p>
+  <p>sss</p>
+  <p>sss</p>
+  <p>sss</p>
+
+  <el-row :gutter="12">
+  
+ 
+  
+    
+    <el-card shadow="hover">
+      <div class="test">
+        <img
+                    src="http://www.axfc.cn/uploads/20190222/8470b0cd14194545b6a04a4768184ef6.jpg"
+                    class="full-width"
+                  >
+        </div>
+    </el-card>
+    
+ 
+  
+</el-row>
+  
+
+  
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {    
+      but4: false
+    };
+  },
+  methods: {
+      addNew0() {
+        this.but0=false;
+
+        this.form1=true;
+        this.but1=true;
+      },
+      
+  },
+  mounted: function() {},
+  updated: function() {}
+};
+</script>
+
+<style scoped>
+.test {
+  height:300px;
+  width:100px;
+}
+.card {
+   width:100px;
+}
+</style>
+
+
