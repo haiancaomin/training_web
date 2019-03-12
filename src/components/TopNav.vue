f941da7ca233817aeb83b190d7fc19c5da40ffc8
yanweiyuan <2569302699@qq.com>
2019-03-12 10:27:23 +0800

    隐藏导航栏人力资源服务按钮

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -59,9 +59,9 @@
         <el-menu-item index="5">
           <a href="javascript:;">资讯</a>
         </el-menu-item>
-        <el-menu-item index="/HRServiceTypeChoice">
+        <!-- <el-menu-item index="/HRServiceTypeChoice">
           <a href="javascript:;">人力资源服务</a>
-        </el-menu-item>
+        </el-menu-item> -->
         <el-menu-item index="/download">
           <a href="javascript:;">资料下载</a>
         </el-menu-item>
