0daf10d85cc91430119e584c4b2f2db12a34eff4
haiancaomin <haiancaomin@126.com>
2019-03-12 09:50:20 +0800

    导航主题色

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -9,7 +9,7 @@
         class="el-menu-demo"
         mode="horizontal"
         @select="handleSelect"
-        background-color="#0a6998"
+        background-color="#242e68"
         text-color="#fff"
         active-text-color="#ffd04b"
         router
@@ -91,7 +91,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .el-header {
-  background: #0a6998;
+  background: #242e68;
   color: #fff;
   line-height: 60px;
   position: fixed;
