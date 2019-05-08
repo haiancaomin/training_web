9a6be7a92112231dcef74137cc567ebda7bace42
haiancaomin <haiancaomin@126.com>
2019-05-08 17:38:03 +0800

    no message

--- src/page/base/baseShow1.vue
+++ src/page/base/baseShow1.vue
@@ -2,7 +2,7 @@
   <div class="base-box">
     <el-collapse v-model="activeNames" @change="handleChange" v-if="swipershow">
       <el-collapse-item :title="baseInfo.basename" name="1" class="base-intro">
-        <swiper :options="swiperOption1" ref="mySwiper1">
+        <swiper :options="swiperOption" ref="mySwiper">
           <!-- slides -->
           <swiper-slide v-for="(item,key) in baseInfo.pictureUrl" :key="key">
             <img :src="item" class="base-intro-pic">
@@ -11,39 +11,22 @@
         <div class="base-content base-show-intro">{{baseInfo.summary}}</div>
       </el-collapse-item>
       <el-collapse-item
-        :title="areaList0.areaname"
-        name="2"
-        class="guanjiang"
-        v-if="JSON.stringify(areaList0)!='{}'"
-      >
-        <el-carousel :interval="5000">
-          <el-carousel-item v-for="(item,key) in areaList0.areapictureUrl" :key="key">
-            <img :src="item" class="guanjiang-pic">
-          </el-carousel-item>
-          <div class="guanjiang-content">
-            <!-- <h1>Title</h1>
-            <h2>Subtitle</h2>-->
-            <p>{{areaList0.area_summary}}</p>
-          </div>
-        </el-carousel>
-      </el-collapse-item>
-      <el-collapse-item
         :title="item.areaname"
-        :name="key+3"
-        class="quality-worker"
-        v-for="(item,key) in arealistrest"
+        :name="key+2"
+        class="guanjiang"
+        v-for="(item,key) in baseInfo.arealist"
         :key="key"
       >
-        <div class="quality-worker-pic clearfix">
-          <swiper :options="swiperOption[key]" :ref="'mySwiper'+(key+2)">
-            <!-- slides -->
-            <swiper-slide v-for="(v,k) in item.areapictureUrl" :key="k">
-              <img :src="v">
-            </swiper-slide>
-            <div class="swiper-pagination" slot="pagination"></div>
-          </swiper>
+        <el-carousel :interval="5000">
+          <el-carousel-item v-for="(v,k) in item.areapictureUrl" :key="k">
+            <img :src="v" class="guanjiang-pic">
+          </el-carousel-item>
+        </el-carousel>
+        <div class="guanjiang-content">
+          <!-- <h1>Title</h1>
+          <h2>Subtitle</h2>-->
+          {{item.area_summary}}
         </div>
-        <div class="quality-worker-content">{{item.area_summary}}</div>
       </el-collapse-item>
     </el-collapse>
   </div>
@@ -57,7 +40,7 @@ export default {
       areaList0: {},
       arealistrest: [],
       swipershow: false,
-      swiperOption1: {
+      swiperOption: {
         autoplay: {
           disableOnInteraction: false
         },
@@ -75,40 +58,14 @@ export default {
           modifier: 1,
           slideShadows: true
         }
-      },
-      swiperOption2: {
-        autoplay: {
-          disableOnInteraction: false
-        },
-        observer: true,
-        observeParents: true,
-        loop: true,
-        effect: "flip",
-        grabCursor: true,
-        pagination: {
-          el: ".swiper-pagination"
-        }
-      },
-      swiperOption3: {
-        autoplay: {
-          disableOnInteraction: false
-        },
-        observer: true,
-        observeParents: true,
-        loop: true,
-        effect: "cube",
-        grabCursor: true,
-        pagination: {
-          el: ".swiper-pagination"
-        }
       }
     };
   },
-  computed: {
-    swiperOption: function() {
-      return [this.swiperOption2, this.swiperOption3];
-    }
-  },
+  // computed: {
+  //   swiperOption: function() {
+  //     return [this.swiperOption2, this.swiperOption3];
+  //   }
+  // },
   props: ["id"],
   mounted() {
     this.getBaseInfoById();
@@ -127,12 +84,12 @@ export default {
       })
         .then(res => {
           this.baseInfo = res.data.data;
-          this.areaList0 = res.data.data.arealist.length
-            ? res.data.data.arealist[0]
-            : {};
-          this.arealistrest = res.data.data.arealist.length
-            ? res.data.data.arealist.slice(1)
-            : [];
+          // this.areaList0 = res.data.data.arealist.length
+          //   ? res.data.data.arealist[0]
+          //   : {};
+          // this.arealistrest = res.data.data.arealist.length
+          //   ? res.data.data.arealist.slice(1)
+          //   : [];
           this.swipershow = true;
         })
         .catch(function(err) {
@@ -165,12 +122,13 @@ export default {
   width: 100%;
   height: 100%;
 }
-.base-show-intro{
+.base-show-intro {
   text-indent: 30px;
   line-height: 25px;
 }
 .guanjiang-pic {
   width: 100%;
+  height: 300px;
 }
 .base-intro .swiper-container {
   width: 100%;
@@ -179,19 +137,6 @@ export default {
 .base-content {
   padding-top: 30px;
 }
-.guanjiang-content {
-  color: #555;
-  position: absolute;
-  top: 40px;
-  left: 60px;
-  background: rgba(255, 255, 255, 0.4);
-  z-index: 100;
-  border-radius: 5px;
-  transition: all linear 0.3s;
-}
-.guanjiang-content:hover {
-  background: rgba(255, 255, 255, 1);
-}
 
 /* .guanjiang-content h1 {
   font-size: 41px;
@@ -199,37 +144,19 @@ export default {
 .guanjiang-content h2 {
   font-size: 14px;
 } */
-.guanjiang-content p {
-  max-width: 350px;
+.guanjiang-content{
   padding: 5px 10px;
-}
-.quality-worker img {
-  width: 100%;
-  height: 100%;
-}
-.quality-worker .swiper-container {
-  float: left;
-  position: relative;
-  padding: 50px;
-  width: 400px;
-  height: 400px;
-}
-.quality-worker-pic {
-  float: left;
 }
 .clearfix:after {
   content: "";
   display: block;
   clear: both;
 }
-.quality-worker-content {
-  padding-top: 50px;
-}
 </style>
 <style>
 .base-box .el-collapse-item__header {
   font-size: 17px;
-  height:70px;
+  height: 70px;
   line-height: 70px;
 }
 .base-box .el-collapse-item__content {
