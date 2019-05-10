8fc28d72dc90f8b509554675f418000f26ab255f
yanwei_zz <2569302699@qq.com>
2019-05-10 16:35:48 +0800

    资料下载UI

--- src/page/downloads/fileList.vue
+++ src/page/downloads/fileList.vue
@@ -4,10 +4,21 @@
       <h1 class="file-title">文件列表</h1>
       <ul class="file-box">
         <li v-for="(item,key) in fileLists.data" :key="key" class="clearfix">
-          <span v-if="item.name.split('.')[1] == 'doc'||item.name.split('.')[1] == 'docx'"><i class="iconfont">&#xe64e;</i></span>
-          <span v-if="item.name.split('.')[1] == 'pdf'"><i class="iconfont">&#xe740;</i></span>
+          <div>
+          <span v-if="item.name.split('.')[1] == 'doc'||item.name.split('.')[1] == 'docx'"><i class="iconfont doc_icon">&#xe64e;</i></span>
+          <span v-if="item.name.split('.')[1] == 'pdf'"><i class="iconfont pdf_icon">&#xe740;</i></span>
           <a :href="item.fileurl">{{item.name}}</a>
-          <span class="date">{{item.createdate}}</span>
+          </div>
+          <div class="update_date">
+            <span class="date">更新时间：{{item.createdate}}</span>
+          </div>
+
+          <div class="download">
+            <a :href="item.fileurl">
+            <el-button type="primary" class="download_btn"><i class="iconfont">&#xe682;</i>下载</el-button>
+            </a>
+          </div>
+          
         </li>
       </ul>
       <el-row>
@@ -83,22 +94,33 @@ export default {
   border-radius: 3px;
 }
 .file-box {
-  padding-top: 30px;
+  padding: 30px 30px 0px 30px;
+}
+.file-box li {
+  margin:10px 0px 0px 0px;
+  border-bottom: 1px dashed  #9e9e9e;
+  padding:0px 10px;
 }
 
 .file-box li a {
   line-height: 30px;
-  color: #333;
+  color: #3e3e3e;
+  font-size:14px;
+  font-weight: bold;
 }
 .file-box li a:hover {
   color: #409eff;
 }
 .date {
-  float: right;
   color: #999;
+  font-size:12px;
+}
+.update_date {
+  margin:0px 0px 10px 0px;
 }
 .text-right {
   text-align: right;
+  padding:0px 30px 0px 0px;
 }
 .clearfix:after{
   content: '';
@@ -107,12 +129,12 @@ export default {
 }
 @font-face {
   font-family: 'iconfont';  /* project id 1131189 */
-  src: url('//at.alicdn.com/t/font_1131189_cto7yxkxej.eot');
-  src: url('//at.alicdn.com/t/font_1131189_cto7yxkxej.eot?#iefix') format('embedded-opentype'),
-  url('//at.alicdn.com/t/font_1131189_cto7yxkxej.woff2') format('woff2'),
-  url('//at.alicdn.com/t/font_1131189_cto7yxkxej.woff') format('woff'),
-  url('//at.alicdn.com/t/font_1131189_cto7yxkxej.ttf') format('truetype'),
-  url('//at.alicdn.com/t/font_1131189_cto7yxkxej.svg#iconfont') format('svg');
+  src: url('//at.alicdn.com/t/font_1131189_aqgbpfdqk5t.eot');
+  src: url('//at.alicdn.com/t/font_1131189_aqgbpfdqk5t.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_1131189_aqgbpfdqk5t.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_1131189_aqgbpfdqk5t.woff') format('woff'),
+  url('//at.alicdn.com/t/font_1131189_aqgbpfdqk5t.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_1131189_aqgbpfdqk5t.svg#iconfont') format('svg');
 }
 .iconfont {
   font-family: "iconfont" !important;
@@ -121,6 +143,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -webkit-text-stroke-width: 0.2px;
   -moz-osx-font-smoothing: grayscale;
+}
+.pdf_icon {
+  color:#F56C6C;
+  font-size:16px;
+}
+.doc_icon {
+  color:#409EFF;
+}
+.download {
+  position: absolute;
+  margin:-52px 0px 0px 765px;
+}
+.download_btn {
+  height:35px;
+  width:70px;
+  padding: 0px;
 }
 </style>
 
